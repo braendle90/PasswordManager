337d8ad05b48b4d4282220379ca651390d159247
dominik <d.braendle@aub.at>
2021-05-20 19:58:05 +0200

    add ReadFile and writeFile with working code for read and write

--- src/com/company/ReadFile.java
+++ src/com/company/ReadFile.java
@@ -1,4 +1,39 @@
 package com.company;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ReadFile {
+
+    //String fileLocation = "src/com/company/database.txt";
+    private String currentLine;
+    private String findID;
+    private List<String> loginInformation;
+
+    public ReadFile(int findID) {
+        this.findID = Integer.toString(findID);
+    }
+
+    public List<String> getFileFromDatabase() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(Main.fileLocation));
+
+        while ((currentLine = reader.readLine()) != null) {
+            if (currentLine.contains(findID)) {
+                loginInformation = Arrays.asList(currentLine.split(","));
+                //System.out.println("Zeile ist: " + loginInformation);
+
+            }
+        }
+
+        reader.close();
+
+        return loginInformation;
+
+
+    }
+
 }
--- src/com/company/WriteFile.java
+++ src/com/company/WriteFile.java
@@ -1,4 +1,31 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 public class WriteFile {
+
+    private String loginInformation;
+
+    public WriteFile(String loginInformation) {
+        this.loginInformation = loginInformation;
+    }
+
+    public void writeInformationToFile() throws IOException {
+
+
+        FileWriter writer = new FileWriter(Main.fileLocation, StandardCharsets.UTF_8, true);
+
+        writer.write("\n" + loginInformation);
+
+        writer.close();
+
+
+
+    }
+
+
+
 }
