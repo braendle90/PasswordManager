5c5b3d1820bf40c8c2ed72f04735d072d4b01bf0
m3xpl4y <m3xpl4y@gmail.com>
2021-05-20 21:55:46 +0200

    main frame and button added

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -1,4 +1,23 @@
 package com.company;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+
+    private JPanel Background;
+    private JButton button1;
+
+    public MainFrame(){
+        super("Haupt Fenster");
+        this.setContentPane(this.Background);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+        mf.setSize(800,600);
+
+    }
 }
