ce436cba6c7da10725ab1f2ba7115e57be2fad67
m3xpl4y <m3xpl4y@gmail.com>
2021-05-20 21:49:02 +0200

    form

--- src/com/company/CreateNewPassword.java
+++ src/com/company/CreateNewPassword.java
@@ -1,4 +1,30 @@
 package com.company;
 
-public class CreateNewPassword {
+import javax.swing.*;
+import java.awt.*;
+
+public class CreateNewPassword extends javax.swing.JFrame{
+    private JPanel Background;
+    private JButton button1;
+    private JButton button2;
+    private JTextField TITLETextField;
+    private JTextField USERNAMETextField;
+    private JTextField PASSWORTTextField;
+    private JTextField WEBSITETextField;
+    private JLabel lbl_title;
+    private JLabel lbl_username;
+    private JLabel lbl_password;
+    private JLabel lbl_website;
+
+    public CreateNewPassword(){
+        super("test");
+        this.setContentPane(this.Background);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        CreateNewPassword createNewPassword = new CreateNewPassword();
+        createNewPassword.setSize(800,600);
+        createNewPassword.setVisible(true);
+    }
 }
