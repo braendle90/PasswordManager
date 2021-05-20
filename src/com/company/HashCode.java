d1ff94bef5031471b5f40cabdc5f0d4b3e314c75
m3xpl4y <m3xpl4y@gmail.com>
2021-05-20 19:48:42 +0200

    hash done

--- src/com/company/HashCode.java
+++ src/com/company/HashCode.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashCode {
 
-    public static void main(String[] args) {
-        System.out.println("Dies ist ein Test!");
+    public String setSecurePassword(String pw) throws NoSuchAlgorithmException {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(pw.getBytes("utf-8"));
+            StringBuffer hexString = new StringBuffer();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append("0");
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        HashCode hc = new HashCode();
+        System.out.println(hc.setSecurePassword("max"));
     }
 }
